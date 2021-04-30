9970aabf452f95fc68648aa70c265e4d33d98c90
ankj17 <44611254+ankj17@users.noreply.github.com>
2021-04-30 12:25:28 +0200

    Friction coefficient update
    Implemented jacobian analytically to improve the  computation time

--- estimation/example_friction_coefficient_estimation.m
+++ estimation/example_friction_coefficient_estimation.m
@@ -2,7 +2,7 @@
 clear;
 clc;
 close all;
-[forces_tcp, moments_tcp, vel_tcp, F_friction_GT, F_normal_GT, mu_static_GT, mu_dyn_GT, label] = loadRawData('rawData/Copper_on_cast_iron.mat', 8, 0);
+[forces_tcp, moments_tcp, vel_tcp, F_friction_GT, F_normal_GT, mu_static_GT, mu_dyn_GT, label] = loadRawData('rawData/Mild_steel_on_lead.mat', 8, 0);
 
 %%
 [mu_static, mu_dynamic, stribeck, nabla] = estimateFrictionCoefficients(forces_tcp, moments_tcp, vel_tcp);
